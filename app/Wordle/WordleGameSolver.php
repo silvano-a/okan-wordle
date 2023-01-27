d96aa8ad6a632c29c9d0330d2db65f793fa11778
Silvano Allemekinders <s.allemekinders@gmail.com>
2023-01-27 14:31:35 +0100

    solver

--- app/Wordle/WordleGameSolver.php
+++ app/Wordle/WordleGameSolver.php
@@ -7,12 +7,66 @@ class WordleGameSolver
     /** @var Game */
     private $game;
 
+    private $guesses = [];
+
+    private $possibleWords = [];
+
     public function start(Game $game)
     {
         $this->game = $game;
+        $this->possibleWords = $game->wordlist;
 
-        dd(
-            $this->game->guessRow('whack') // make guess that the word is 'whack'. returns validation Row::class object.
-        );
+        $row = $this->game->guessRow("adore");
+
+        $this->guesses []= $row;
+
+        while (true) {
+            $this->analyseWords($row);
+            $row = $this->game->guessRow(reset($this->possibleWords));
+            $this->guesses []= $row;
+        }
+
+
+        //dd($this->guesses);
+
+
+    }
+
+    private function analyseWords(Row $row)
+    {
+        foreach ($row->getArrayWord() as $key => $arrayLetter) {
+            if ($arrayLetter['position'] === Row::POSITION_CORRECT) {
+                $this->filterIsCorrectLetter($arrayLetter['letter'], $key);
+            }
+
+            if ($arrayLetter['position'] === Row::POSITION_NONE) {
+                $this->filterIsNoneLetter($arrayLetter['letter'], $key);
+            }
+
+            if($arrayLetter['position'] === Row::POSITION_WRONG) {
+                $this->filterHasCorrectLetter($arrayLetter['letter']);
+                $this->filterIsNoneLetter($arrayLetter['letter'], $key);
+            }
+        }
+    }
+
+    private function filterHasCorrectLetter(string $letter) {
+        $this->possibleWords = array_filter($this->possibleWords, function (string $word) use ($letter) {
+            return strpos($word, $letter) !== false;
+        });
+    }
+
+    private function filterIsCorrectLetter(string $letter, int $position): void
+    {
+        $this->possibleWords = array_filter($this->possibleWords, function (string $word) use ($letter, $position) {
+            return $word[$position] === $letter;
+        });
+    }
+
+    private function filterIsNoneLetter(string $letter, int $position)
+    {
+        $this->possibleWords = array_filter($this->possibleWords, function (string $word) use ($letter, $position) {
+            return $word[$position] !== $letter;
+        });
     }
 }
